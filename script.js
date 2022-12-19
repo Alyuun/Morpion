7ac5790558fbdc79574c7821bdb6f138a3c7f1a8
Alyuun <alizeyun@gmail.com>
2022-12-19 09:43:52 +0000

    création des 3 fichiers de base html css et js + création des variables et recupération en js

---
+++ script.js
@@ -0,0 +1,29 @@
+window.addEventListener("DOMContentLoaded", () => {
+// récupération des div du html pour les insérer dans 3 tableaux qui représentent 3 lignes de 3 cases chacuns
+let firstLineTab = document.getElementsByClassName("firstLine");
+let secondLineTab = document.getElementsByClassName("secondLine");
+let thirdLineTad = document.getElementsByClassName("thirdLine");
+
+console.log(firstLineTab);
+// création des 2 variables player
+let xPlayer = "";
+let oPlayer = "";
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+})
