d611db2290cda3eed92176a94cfd902d25d35813
Alyuun <alizeyun@gmail.com>
2022-12-19 11:23:19 +0000

    lalal

--- script.js
+++ script.js
@@ -1,38 +1,60 @@
 window.addEventListener("DOMContentLoaded", () => {
-// récupération des div du html pour les insérer dans 3 tableaux qui représentent 3 lignes de 3 cases chacuns
+// récupération des div du html pour les insérer dans 3 tableaux qui représentent 3 lignes de 3 cases chaccase1s
 let firstColumnTab = document.getElementsByClassName("firstLine");
 let secondColumnTab = document.getElementsByClassName("secondLine");
 let thirdColumnTad = document.getElementsByClassName("thirdLine");
+let nbTours = 0;
 
-let un = document.getElementById("1");
-let deux = document.getElementById("2");
-let trois = document.getElementById("3");
-let quatre = document.getElementById("4");
-let cinq = document.getElementById("5");
-let six = document.getElementById("6");
-let sept = document.getElementById("7");
-let huit = document.getElementById("8");
-let neuf = document.getElementById("9");
+let case1 = document.getElementById("1");
+let case2 = document.getElementById("2");
+let case3 = document.getElementById("3");
+let case4 = document.getElementById("4");
+let case5 = document.getElementById("5");
+let case6 = document.getElementById("6");
+let case7 = document.getElementById("7");
+let case8 = document.getElementById("8");
+let case9 = document.getElementById("9");
+
+let startPlayer = " ";
 
 console.log(firstColumnTab);
 // création des 2 variables player
-let xPlayer = "";
-let oPlayer = "";
+let xPlayer = "X";
+let oPlayer = "O";
 
 /*for (let i = 0; i < 9; i++) {
     xPlayer = 0;
     oPlayer = 1;
 }*/
 
+function quiCommence() {
+    startPlayer = Math.floor(Math.random() * (3 - 1) + 1)
+    if (startPlayer === 1) {
+        startPlayer = xPlayer;
+    } else if (startPlayer === 2) {
+        startPlayer = oPlayer;
+    }
+}
 
+quiCommence();
+console.log(`C'est joueur ${startPlayer} qui commence !`)
 
-if ((un === "x" && deux === "x" && trois === "x") || (quatre === "x" && cinq === "x" && six === "x") || (sept === "x" && huit === "x" && neuf === "x") || (un === "x" && quatre === "x" && sept === "x") || (deux === "x" && cinq === "x" && huit === "x") || (trois === "x" && six === "x" && neuf === "x")) {
+let cases = document.getElementsByClassName("container")
+console.log(cases)
+
+cases.addEventListener("click", () => {
+    if (actualPlayer = XPlayer) {
+        
+    }
+})
+
+if ((case1 === "x" && case2 === "x" && case3 === "x") || (case4 === "x" && case5 === "x" && case6 === "x") || (case7 === "x" && case8 === "x" && case9 === "x") || (case1 === "x" && case4 === "x" && case7 === "x") || (case2 === "x" && case5 === "x" && case8 === "x") || (case3 === "x" && case6 === "x" && case9 === "x")) {
     alert("Jouer X tu as gagné");
-} else if ((un === "o" && deux === "o" && trois === "o") || (quatre === "o" && cinq === "o" && six === "o") || (sept === "o" && huit === "o" && neuf === "o") || (un === "o" && quatre === "o" && sept === "o") || (deux === "o" && cinq === "o" && huit === "o") || (trois === "o" && six === "o" && neuf === "o")) {
+} else if ((case1 === "o" && case2 === "o" && case3 === "o") || (case4 === "o" && case5 === "o" && case6 === "o") || (case7 === "o" && case8 === "o" && case9 === "o") || (case1 === "o" && case4 === "o" && case7 === "o") || (case2 === "o" && case5 === "o" && case8 === "o") || (case3 === "o" && case6 === "o" && case9 === "o")) {
     alert("Joueur O tu as gagné")
 }
 
-    
+// 
     
     
     
@@ -51,3 +73,9 @@ button.addEventListener("click", ()=> {
 })
     
 })
+
+
+// Joueur X ou O qui commence aléatoirement. Quand je clique sur une case si c'est joueur X la case innerHTML X si c'est joueur O la case innerHTML O
+// si 3 cases de suite sont X alors joueur X a gagné 
+// si 3 cases de suite sont O alors joueur O a gagné
+
